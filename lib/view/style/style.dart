bd08f85b74f1ac129cdb42af2d4a69b12a912b0b
220278cs03 <220278@akfauniversity.org>
2023-02-11 13:48:04 +0500

    added - style

--- lib/view/style/style.dart
+++ lib/view/style/style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Style {
   Style._();
@@ -15,4 +16,39 @@ class Style {
   //------------------------ MainStyle -----------------------------------------//
 
   static const whiteColor = Color(0xffFFFFFF);
+  static const blackColor = Color(0xff09101D);
+  static const primaryBlue = Color(0xff2972FE);
+
+  //------------------------ CategoryStyle -------------------------------------//
+
+  static const bgCategory = Color(0x102972FE);
+  static const borderCategory = Color(0xffEBEEF2);
+
+  //----------------------- SearchStyle ----------------------------------------//
+
+  static const searchBgColor = Color(0xffF4F6F9);
+  static const searchIconColor = Color(0xff858C94);
+
+  //----------------------- TextColor ------------------------------------------//
+
+  static const titleTextColor = Color(0xff2C3A4B);
+  static const ratingTextColor = Color(0xff394452);
+
+  //----------------------- TextStyle ------------------------------------------//
+
+  static textStyleThin({
+    double size = 14,
+    Color textColor = blackColor,
+  }) {
+    return GoogleFonts.sourceSansPro(
+        fontWeight: FontWeight.w400, color: textColor, fontSize: size);
+  }
+
+  static textStyleRegular({
+    double size = 14,
+    Color textColor = blackColor,
+  }) {
+    return GoogleFonts.sourceSansPro(
+        fontWeight: FontWeight.w600, color: textColor, fontSize: size);
+  }
 }
